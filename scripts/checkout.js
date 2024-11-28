b001fb01fcb4336c3721e5c69c0afb50910e870c
estebonbon <estesarmiento1@gmail.com>
2024-11-27 20:32:26 -0500

    create class conversion of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,7 +3,7 @@ import { renderOrderSummary } from './checkout/ordersummary.js';
 
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 renderCheckoutHeader();
 renderPaymentSummary();
